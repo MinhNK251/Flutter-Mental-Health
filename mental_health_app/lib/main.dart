619cd6f3c569d4c9ff7908fe2b125a2bfab326b1
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-08-22 08:47:12 +0700

    Refactor onboarding.dart

--- mental_health_app/lib/main.dart
+++ mental_health_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mental_health_app/presentation.onboarding/onboarding.dart';
+import 'package:mental_health_app/presentation/onboarding/onboarding.dart';
 import 'core/theme.dart';
 
 void main() {
